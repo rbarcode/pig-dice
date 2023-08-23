fc53d0731f0ce05a9f4118edd6269e1bf8f996af
Richard Barbour <richyb07@gmail.com>
2023-08-23 13:48:25 -0700

    Pass test 4
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,14 @@ function Player() {
 
 function Game(player1, player2) {
   this.players = [player1 = new Player(player1), player2 = new Player(player2)];
-  this.totalScore = [player1.totalScore, player2.totalScore]
+  this.totalScore = [player1.totalScore, player2.totalScore];
   this.activePlayer = this.players[0];
   this.die = 0;
   this.gameOver = false;
+}
+
+Game.prototype.dieRoll = function () {
+  let currentRollValue = Math.floor(Math.random() * 6) + 1;
+  return currentRollValue;
+
 }
