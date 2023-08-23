7f78def62f90276f76f35b4838eba7ec77a94274
Richard Barbour <richyb07@gmail.com>
2023-08-23 13:34:00 -0700

    Pass test 3
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ function Player() {
   this.currentScore = 0;
   this.isTurn = false;
 }
+
+
+function Game(player1, player2) {
+  this.players = [player1 = new Player(player1), player2 = new Player(player2)];
+  this.totalScore = [player1.totalScore, player2.totalScore]
+  this.activePlayer = this.players[0];
+  this.die = 0;
+  this.gameOver = false;
+}
