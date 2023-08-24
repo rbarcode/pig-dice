5c0d6f20c89ed9d1b325e76ea96a21e9e09a7c29
Richard Barbour <richyb07@gmail.com>
2023-08-24 10:25:20 -0700

    Instantiate the player objects from within the game constructor.
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,54 @@
+//Business Logic
+//Player Constructor ------------
 function Player() {
   this.totalScore = 0;
   this.currentScore = 0;
-  this.isTurn = false; //see if we need this 
 }
 
-// Player.prototype.calcCurrentScore = function (currentRollValue) {
-//   if (currentRollValue === 1) {
-//     this.currentScore = 0;
-//     Game.changeTurn();
-//   } else {
-//     this.currentScore += currentRollValue;
-//   }
-//   return this.currentScore;
-// }
-
-Player.prototype.calcTotalScore = function () {
-  this.totalScore += this.currentScore;
-  return this.totalScore;
-}
-
+//Game Constructor ------------
 function Game(player1, player2) {
   this.players = [player1 = new Player(player1), player2 = new Player(player2)];
-  this.totalScore = [player1.totalScore, player2.totalScore];
-  this.activePlayer = this.players[0];
+  this.activePlayer = this.players[0];pio/"("
   this.currentRollValue = 0;
   this.gameOver = false;
 }
 
-Game.prototype.dieRoll = function () {
-  this.currentRollValue = Math.floor(Math.random() * 6) + 1;
-  // this.activePlayer.calcCurrentScore(this.currentRollValue);
-  // return this.currentRollValue;
+Game.prototype.calcCurrentScore = function () {
   if (this.currentRollValue === 1) {
     this.activePlayer.currentScore = 0;
     this.changeTurn();
   } else {
-    this.activePlayer.currentScore += this.currentRollValue; 
+    this.activePlayer.currentScore += this.currentRollValue;
+  }
+  return this.activePlayer.currentScore;
+}
+
+Game.prototype.calcTotalScore = function () {
+  this.activePlayer.totalScore += this.activePlayer.currentScore;
+  return this.totalScore;
+}
+
+Game.prototype.dieRoll = function () {
+  this.currentRollValue = Math.floor(Math.random() * 6) + 1;
+  this.calcCurrentScore(this.currentRollValue);
   return this.currentRollValue;
 }
 
 Game.prototype.changeTurn = function () {
-
   if (this.activePlayer === this.players[0]) {
     this.activePlayer = this.players[1]
   } else {
     this.activePlayer = this.players[0]
   }
-  this.players.forEach(function (player) {
-    player.isTurn = !player.isTurn //should only have one true... it changing both to true.
-  });
-
 }
 
-// Game.prototype.hold = function () {
-//   this.activePlayer.calcTotalScore();
-// }
+Game.prototype.hold = function () {
+  this.calcTotalScore();
+  this.activePlayer.currentScore = 0;
+  this.changeTurn();
+}
 
-//this.activePlayer.changeTurn();
-// this.activePlayer.isTurn = true
-// this.activePlayer = 
+
+
+
+ //User Interface Logic
